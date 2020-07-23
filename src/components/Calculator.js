d3d157700d8334955fd08ae984807e3d22319426
Enrollment Computer <63064941+token-hub@users.noreply.github.com>
2020-07-23 13:27:29 +0800

    append new numbers to left of the current value done

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,12 +10,21 @@ class Calculator extends React.Component {
 		}
 	}
 
-	changeHandler = event => this.setState({ num: event.target.value })
-
-	numberClickHandler = event => this.setState({ num: event.target.value })
-	
 	clearHandler = () => this.setState({ num: 0 })
 
+	changeHandler = event => this.setState({ num: event.target.value })
+
+	numberAndOperatorClickHandler = event => {
+		let result = this.state.num === 0 ? event.target.value : this.state.num + event.target.value ;
+		this.setState({ num: result })
+	}
+
+	equalsOperationHandler = () => {
+		// slice the last character
+		// check if its a number if not display error message
+		console.log('equals');
+	}
+	
 	render() {
 		return (
 			<div className='calculator'>
@@ -31,56 +40,56 @@ class Calculator extends React.Component {
 							</div>
 							<div className="right second">
 								<div className="right-item">
-									<button className='something-red'> = </button>
+									<button className='something-red' onClick={this.equalsOperationHandler} value={'='}> = </button>
 								</div>
 								<div className="right-item something-red">
-									<button className='something-red'> + </button>
+									<button className='something-red' onClick={this.numberAndOperatorClickHandler} value={'+'}> + </button>
 								</div>
 							</div>
 						</div>
 
 						<div className='content'>
 							<div className="item">
-								<button value={7} onClick={this.numberClickHandler}> 7 </button>
+								<button value={7} onClick={this.numberAndOperatorClickHandler}> 7 </button>
 							</div>
 							<div className="item">
-								<button value={8} onClick={this.numberClickHandler}> 8 </button>
+								<button value={8} onClick={this.numberAndOperatorClickHandler}> 8 </button>
 							</div>
 							<div className="item">
-								<button value={9} onClick={this.numberClickHandler}> 9 </button>
+								<button value={9} onClick={this.numberAndOperatorClickHandler}> 9 </button>
 							</div>
 							<div className="item something-red">
-								<button className='something-red'> - </button>
+								<button className='something-red' onClick={this.numberAndOperatorClickHandler} value={'-'}> - </button>
 							</div>
 						</div>
 
 						<div className='content'>
 							<div className="item">
-								<button value={4} onClick={this.numberClickHandler}> 4 </button>
+								<button value={4} onClick={this.numberAndOperatorClickHandler}> 4 </button>
 							</div>
 							<div className="item">
-								<button value={5} onClick={this.numberClickHandler}> 5 </button>
+								<button value={5} onClick={this.numberAndOperatorClickHandler}> 5 </button>
 							</div>
 							<div className="item">
-								<button value={6} onClick={this.numberClickHandler}> 6 </button>
+								<button value={6} onClick={this.numberAndOperatorClickHandler}> 6 </button>
 							</div>
 							<div className="item something-red">
-								<button className='something-red'> * </button>
+								<button className='something-red' onClick={this.numberAndOperatorClickHandler} value={'*'}> * </button>
 							</div>
 						</div>
 
 						<div className='content'>
 							<div className="item">
-								<button value={1} onClick={this.numberClickHandler}> 1 </button>
+								<button value={1} onClick={this.numberAndOperatorClickHandler}> 1 </button>
 							</div>
 							<div className="item">
-								<button value={2} onClick={this.numberClickHandler}> 2 </button>
+								<button value={2} onClick={this.numberAndOperatorClickHandler}> 2 </button>
 							</div>
 							<div className="item">
-								<button value={3} onClick={this.numberClickHandler}> 3 </button>
+								<button value={3} onClick={this.numberAndOperatorClickHandler}> 3 </button>
 							</div>
 							<div className="item something-red">
-								<button className='something-red'> / </button>
+								<button className='something-red' onClick={this.numberAndOperatorClickHandler} value={'/'}> / </button>
 							</div>
 						</div>
 					</div>
